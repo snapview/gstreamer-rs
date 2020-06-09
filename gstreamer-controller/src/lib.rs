84740f44478e01ef68a11341f8f7a2450d1b70a9
Alexey Galakhov <agalakhov@snapview.de>
2020-06-09 03:06:49 +0200

    Add gstreamer-controller
    Signed-off-by: Alexey Galakhov <agalakhov@snapview.de>

---
+++ gstreamer-controller/src/lib.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
+extern crate gstreamer as gst;
+extern crate gstreamer_sys as gst_sys;
+
+extern crate gstreamer_controller_sys as gst_controller_sys;
+
+macro_rules! assert_initialized_main_thread {
+    () => {
+        if unsafe { ::gst_sys::gst_is_initialized() } != ::glib_sys::GTRUE {
+            panic!("GStreamer has not been initialized. Call `gst::init` first.");
+        }
+    };
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+#[allow(clippy::unreadable_literal)]
+#[allow(clippy::too_many_arguments)]
+#[allow(clippy::match_same_arms)]
+mod auto;
+pub use auto::*;
+
+pub mod prelude {
+    pub use auto::traits::*;
+}
